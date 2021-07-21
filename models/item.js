00d6962d82f6a2b9af28ebebe7df1392d82855e4
c <rladuddlf3@naver.com>
2021-07-21 15:32:26 +0900

    물품 등록하기 API 구현

--- models/item.js
+++ models/item.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const itemSchema = new mongoose.Schema({
+  itemImage: {
+    type:String,
+    required: true
+  },
   itemName:{
     type:String,
     unique:true,
